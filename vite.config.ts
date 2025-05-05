424bc8d6adc215d051ec2b19d74e01969428e0e3
Brian Schroeter <bschroeter@tuuci.com>
2025-05-05 12:30:08 -0400

    Updated dependencies, and vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    vercel(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
